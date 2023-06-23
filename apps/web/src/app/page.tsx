5204a198575373ec1cac33f9ab2a1fdef8d6a7c9
udon242 <ylu242@gmail.com>
2023-06-23 18:53:54 +0900

    feat: background color

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,6 +1,7 @@
 import MeterList from '@/app/_components/MeterList';
 import PageTitle from '@/app/_components/PageTitle';
 import { getMeter } from '@/app/service';
+import { css } from '@/styled-system/css';
 import { Container } from '@/styled-system/jsx';
 
 export type Meter = {
@@ -14,7 +15,7 @@ export type Meter = {
 export default async function Home() {
   const meters = await getMeter();
   return (
-    <main>
+    <main className={css({ backgroundColor: 'white' })}>
       <Container paddingX={2}>
         <PageTitle>デバイス一覧</PageTitle>
         <MeterList initialMeterList={meters} />
