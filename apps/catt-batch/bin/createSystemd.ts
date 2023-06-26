6c70b5b9ff2def73691cc63e5645a87c9047c6db
udon242 <ylu242@gmail.com>
2023-06-26 10:11:10 +0900

    chore: add systemd file script

---
+++ apps/catt-batch/bin/createSystemd.ts
@@ -0,0 +1,30 @@
+import { program } from 'commander';
+
+program
+  .requiredOption('--workDir <workDir>', 'working directory')
+  .requiredOption('--device <device>', 'デバイス名 or IPアドレス')
+  .requiredOption('--url <url>', 'URL');
+program.parse(process.argv);
+const opts = program.opts();
+
+const systemd = `
+[Unit]
+Description=CATT batch daemon
+After=network.target
+
+[Service]
+ExecStart=pnpm start
+Restart=always
+User=pi
+# Note Debian/Ubuntu uses 'nogroup', RHEL/Fedora uses 'nobody'
+Group=nogroup
+WorkingDirectory=${opts.workDir}
+Environment=PATH=/usr/bin:/usr/local/bin:~/.local/bin
+Environment=CATT_DEVICE=${opts.device}
+Environment=CATT_CAST_URL=${opts.url}
+
+[Install]
+WantedBy=multi-user.target
+`;
+
+console.info(systemd);
